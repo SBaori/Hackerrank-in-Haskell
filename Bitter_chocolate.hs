d85bbd60bd862a64f2d5a149c99c8931bfb9f1b8
Sahil Baori <96388615+SBaori@users.noreply.github.com>
2023-11-20 01:18:38 +0530

    Update Bitter_chocolate.hs
    added comments

--- Bitter_chocolate.hs
+++ Bitter_chocolate.hs
@@ -1,9 +1,11 @@
 import qualified Data.Map as M 
 import Data.Maybe (fromJust)
 
+-- slice the bar at a coordinate/piece
 getBarStates :: [Int] -> [(Int,Int,Int)]
 getBarStates [r1,r2,r3] = filter (\(x,y,z) -> not $ y==0 && z>0) [(s1,s2,s3) | s1 <- [1..r1], s2 <- [0..r2], s3 <- [0..r3]]
 
+-- generate all possible configuration for a bar
 updateBar :: (Int,Int,Int) -> (Int,Int) -> (Int,Int,Int)
 updateBar (r1,r2,r3) (r,c) = updatedBar
     where
@@ -23,23 +25,25 @@ updateBar (r1,r2,r3) (r,c) = updatedBar
             3 -> if r3 >= c then (r1,r2,c-1) else (r1,r2,r3)
             _ -> (r1,r2,r3)
 
-
+-- for a bar state, check all reachable states in hashMap, or(||) and return the final result.
 helper :: (Int,Int,Int) -> M.Map (Int,Int,Int) Bool -> Bool
 helper bar@(r1,r2,r3) hash = foldl (\res c -> res || not (fromJust (M.lookup (updateBar bar c) hash))) False coords 
     where
         coords = [(3,n) | n <- [r3,r3-1..1]] ++ [(2,n) | n <- [r2,r2-1..1]] ++ [(1,n) | n <- [r1,r1-1..2]]
 
-
+-- iterate through all bar states, and update the hash with the call to {helper} (aka bottom up dp solution)
 play :: [(Int,Int,Int)] -> M.Map (Int,Int,Int) Bool
 play barStates = foldl (\newHash bar -> M.insert bar (helper bar newHash) newHash) hash barStates
     where
         hash = M.fromList (map (\x -> (x,False)) barStates)
-
+        
+-- checks the result from {solve} and returns the required string.
 gameRes :: [Int] -> String
 gameRes bar@[r1,r2,r3] = if fromJust $ M.lookup (r1,r2,r3) (play (getBarStates bar)) then 
                             "WIN"
                          else 
                             "LOSE"
+                            
 main :: IO ()
 main = do
     n <- getLine
