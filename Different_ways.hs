aaf6c6c6b043f1a44ec32af05eeb360bae89db1d
Sahil Baori <96388615+SBaori@users.noreply.github.com>
2023-11-23 10:05:49 +0530

    Create Different_ways.hs

---
+++ Different_ways.hs
@@ -0,0 +1,25 @@
+import Data.Array
+
+getRes :: Array Int (Array Int Int)
+getRes = listArray (0,1000) . map rowToArray $ combinations 
+    where
+        rowToArray l = listArray (0, length l - 1) l
+
+-- bottom up dp with the concept nCk = (n-1)Ck + (n-1)C(k-1)
+combinations :: [[Int]]
+combinations = (1:replicate 1000 0) : (nextRow <$> combinations)
+  where 
+    nextRow l@(_:t) = 1 : (zipWith f l t)
+    f x y = (x + y) `mod` (10^8 + 7)
+
+main :: IO ()
+main = do
+    n <- getLine
+    go $ read n
+    where
+        go 0 = return ()
+        go n = do
+            s <- getLine
+            let [nn,k] = map read $ words s
+            print $ getRes ! nn ! k
+            go (n-1)
