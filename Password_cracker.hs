8cd827d265ad7bcecc35df9308b8ea4d9c1cee09
Sahil Baori <96388615+SBaori@users.noreply.github.com>
2024-02-28 04:43:37 +0530

    Create Password_cracker.hs

---
+++ Password_cracker.hs
@@ -0,0 +1,35 @@
+import qualified Data.Map as M
+import qualified Data.Vector as V
+import Data.Maybe (fromMaybe)
+import Data.List (tails, foldl', concatMap)
+
+getPassSplit loginStr pass = V.last dp
+    where
+        loginStrDp = V.fromList $ zip (reverse $ tails loginStr) [0..]
+        charMap = foldl' (\cm str@(c:cs) -> M.insertWith (++) c [(str, length str)] cm) M.empty pass
+
+        dp = V.generate (length loginStr + 1) genVal
+
+        genVal 0 = [""]
+        genVal i = helper $ loginStrDp V.! i
+            where
+                helper (currLoginStr@(c:cs),len) = 
+                    foldr (\(p,len') res -> if (len >= len') && (p == take len' currLoginStr) && not (null (dp V.! (len-len'))) then 
+                                                p:(dp V.! (len-len')) 
+                                            else 
+                                                res
+                          ) [] $ fromMaybe [] $ M.lookup c charMap
+
+main :: IO ()
+main = do
+    n <- getLine
+    go $ read n
+    where
+        go 0 = return ()
+        go n = do
+            _ <- getLine
+            passStr <- getLine
+            loginStr <- getLine
+            let format a = if null a then "WRONG PASSWORD" else tail $ concatMap (\w -> if null w then w else ' ':w) a
+            putStrLn $ format $ getPassSplit loginStr $ words passStr
+            go (n-1)
