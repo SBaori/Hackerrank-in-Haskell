073768faf7eeb59d3c11b33e8b8f2f23a7e7324b
Sahil Baori <96388615+SBaori@users.noreply.github.com>
2025-03-06 23:24:36 +0530

    Fixed Int overflow bug

--- Sherlock_and_the_Maze.hs
+++ Sherlock_and_the_Maze.hs
@@ -1,25 +1,26 @@
 dp :: [[[(Int,Int)]]]
 dp = initTable : (nextTable <$> dp)
     where
-        initTable = ((1,0) : repeat (1,0)) : repeat ((0,1) : repeat (0,0))
+    initTable = ((1,0) : repeat (1,0)) : repeat ((0,1) : repeat (0,0))
 
-        nextTable :: [[(Int,Int)]] -> [[(Int,Int)]]
-        nextTable prevTable = let t = initRow : zipWith nextRow (zip prevTable (tail prevTable)) t in t
-            where
-                initRow = (0,0) : repeat (1,0)
+    nextTable :: [[(Int,Int)]] -> [[(Int,Int)]]
+    nextTable prevTable = let t = initRow : zipWith nextRow rs t in t
+      where
+        initRow = (0,0) : repeat (1,0)
+        rs = zip prevTable (tail prevTable)
 
-        nextRow :: ([(Int,Int)], [(Int,Int)]) -> [(Int,Int)] -> [(Int,Int)]
-        nextRow (prevTablePrevRow, prevTableCurRow) curTablePrevRow = let r = initCell : zipWith3 nextCell (zip (tail prevTablePrevRow) prevTableCurRow) (tail curTablePrevRow) r in r
-            where
-                initCell = (0,1)
+    nextRow :: ([(Int,Int)], [(Int,Int)]) -> [(Int,Int)] -> [(Int,Int)]
+    nextRow (prevTablePrevRow, prevTableCurrRow) currTablePrevRow = let r = initCell : zipWith3 nextCell (zip (tail prevTablePrevRow) prevTableCurrRow) (tail currTablePrevRow) r in r
+      where
+        initCell = (0,1)
 
-        nextCell :: ((Int,Int), (Int,Int)) -> (Int,Int) -> (Int,Int) -> (Int,Int)
-        nextCell (prevTablePrevRowCell, prevTableCurrRowCell) currTablePrevRowCell currTableCurrRowCell = (b+d,a+c)
-            where
-                a = fst prevTablePrevRowCell
-                b = snd prevTableCurrRowCell
-                c = snd currTablePrevRowCell
-                d = fst currTableCurrRowCell
+    nextCell :: ((Int,Int),(Int,Int)) -> (Int,Int) -> (Int,Int) -> (Int,Int)
+    nextCell (prevTablePrevRowCell, prevTableCurrRowCell) currTablePrevRowCell currTableCurrRowCell = (mod (b+d) 1000000007,mod (a+c) 1000000007)
+      where
+          a = fst prevTablePrevRowCell
+          b = snd prevTableCurrRowCell
+          c = snd currTablePrevRowCell
+          d = fst currTableCurrRowCell
 
 main :: IO ()
 main = do
